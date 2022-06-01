384fa7e0304967773afbdbc8744a0b53e06b9a4d
adik-creator <abdul.usmanov@list.ru>
2022-06-01 12:00:35 +0600

    create form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import {useForm} from "react-hook-form";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const {
+        register,
+        formState: {
+            errors,
+            isValid,
+        },
+        reset,
+        handleSubmit,
+    } = useForm({
+        mode: "onBlur"
+    })
+
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data))
+        reset()
+    }
+
+    return (
+        <div className="formContainer">
+            <h1>Auth</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <label>
+                    First Name
+                    <input type="text"
+                           {...register('firstName', {
+                               required: 'Поле обязательно к зополнению',
+                               minLength: {
+                                   value: 5,
+                                   message: "Минимум 5 символов",
+                               },
+                           })}
+                    />
+                    <div style={{height: 40}}>
+                        {errors?.firstName && <p>{errors?.firstName?.message || "Error!"}</p>}
+                    </div>
+                </label>
+
+                <label>
+                    Last Name
+                    <input type="text"
+                           {...register('lastName', {
+                               required: 'Поле обязательно к зополнению',
+                               minLength: {
+                                   value: 5,
+                                   message: "Минимум 5 символов",
+                               },
+                           })}
+                    />
+                    <div style={{height: 40}}>
+                        {errors?.lastName && <p>{errors?.lastName?.message || "Error!"}</p>}
+                    </div>
+                </label>
+
+                <label>
+                    Password
+                    <input type="password"
+                           {...register('password', {
+                               required: 'Поле обязательно к зополнению',
+                               minLength: {
+                                   value: 8,
+                                   message: "Минимум 8 символов",
+                               },
+                           })}
+                    />
+                    <div style={{height: 40}}>
+                        {errors?.lastName && <p>{errors?.lastName?.message || "Error!"}</p>}
+                    </div>
+                </label>
+                <input type="submit" disabled={!isValid}/>
+            </form>
+        </div>
+    );
 }
 
 export default App;
