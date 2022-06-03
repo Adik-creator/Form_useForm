18558d04328d48992608be98ada73143fcacd88d
adik-creator <abdul.usmanov@list.ru>
2022-06-03 22:56:39 +0600

    used yup

--- src/Form.jsx
+++ src/Form.jsx
@@ -4,18 +4,20 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const schema = yup.object({
+    login: yup.string().required('Поле обязательно к зополнению!'),
+    email: yup.string().email().required('Поле обязательно к зополнению!'),
+    password: yup.string().min(4).max(10).required('Поле обязательно к зополнению!'),
+}).required("Error!");
 
 function Form(props) {
-    const {
-        register,
-        formState: {
-            errors,
-        },
-        reset,
-        handleSubmit,
-    } = useForm({
-        mode: "onBlur"
-    })
+    const { register, handleSubmit, reset, formState:{ errors } } = useForm({
+        resolver: yupResolver(schema),
+        mode: "onBlur",
+    });
 
     const onSubmit = (data) => {
         alert(JSON.stringify(data))
@@ -31,39 +33,32 @@ function Form(props) {
                 component="form" sx={{
                 '& .MuiTextField-root': {m: 1, width: '25ch'},
             }}
-                autoComplete="off"
             >
 
                 <TextField type="text"
                            variant={"outlined"}
-                           label={"Login"}
+                           label={"login"}
                            style={{width: "100%", margin: 0, marginTop: 15}}
                            error={!!errors?.login}
-                           helperText={errors?.login ? errors.login.message : null}
-                           {...register('login', {
-                               required: 'Поле обязательно к зополнению',
-                           })}
+                           helperText={errors?.login && <p>{errors?.login?.message || "Error!"}</p>}
+                           {...register('login')}
                 />
 
                 <TextField type="text"
                            variant={"outlined"}
-                           label={"Email"}
+                           label={"email"}
                            style={{width: '100%', margin: 0, marginTop: 15}}
                            error={!!errors?.email}
-                           helperText={errors?.email ? errors.email.message : null}
-                           {...register('email', {
-                               required: 'Поле обязательно к зополнению',
-                           })}
+                           helperText={errors?.email && <p>{errors?.email?.message || "Error!"}</p>}
+                           {...register('email')}
                 />
                 <TextField type="password"
                            variant={"outlined"}
-                           label={"Password"}
+                           label={"password"}
                            style={{width: "100%", margin: 0, marginTop: 15}}
                            error={!!errors?.Password}
-                           helperText={errors?.Password ? errors.Password.message : null}
-                           {...register('Password', {
-                               required: 'Поле обязательно к зополнению',
-                           })}
+                           helperText={errors?.password && <p>{errors?.password?.message || "Error!"}</p>}
+                           {...register('password')}
                 />
                 <Button variant="contained"
                         type="submit"
@@ -74,9 +69,5 @@ function Form(props) {
         </Container>
     )
 }
-//
-// <div style={{height: 40}}>
-//     {errors?.lastName && <p>{errors?.lastName?.message || "Error!"}</p>}
-// </div>
 
 export default Form;
